d1cf649dc1f4a267fe6a2745000e57e7f58d2f92
Jesse Brior <briorj@gmail.com>
2021-11-07 19:08:56 -0500

    Initial Commit

---
+++ ParsingJSON/Models.swift
@@ -0,0 +1,17 @@
+//
+//  Models.swift
+//  ParsingJSON
+//
+//  Created by Jesse Brior on 11/7/21.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let data: [ResultItem]
+}
+
+struct ResultItem: Codable {
+    let title: String
+    let items: [String]
+}
---
+++ ParsingJSON/ViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  ParsingJSON
+//
+//  Created by Jesse Brior on 11/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var result: Result?
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        parseJSON()
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    // TableView
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return result?.data.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return result?.data[section].title
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let result = result {
+            return result.data[section].items.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let text = result?.data[indexPath.section].items[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = text
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // JSON
+
+    private func parseJSON() {
+        guard let path = Bundle.main.path(forResource: "data", ofType: "json") else {
+            return
+        }
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            let jsonData = try Data(contentsOf: url)
+            result = try JSONDecoder().decode(Result.self, from: jsonData)
+            
+            if let result = result {
+                print(result)
+            } else {
+                print("Failed to parse.")
+            }
+        }
+        catch {
+            print("Error: \(error)")
+        }
+    }
+}
+
